8c9ecbb1e73a96b99802f38aab7bb2a6fd150ac6
unae30 <unae30@gmail.com>
2023-04-03 12:05:22 -0300

    AULA 3 - Melhorando o codigo. Criando classes, interface. Treinando o conceito de refatoração, abstração e POO. Usando API NASA e IMDB

--- src/App.java
+++ src/App.java
@@ -1,61 +1,39 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // fazer uma conexão HTTP e buscar os 250 filmes
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient(); // poderia usar var que o java reconhece
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-
-        // extrair os dados que interessam (titulo, poster, classificação) ---//nao usei
-        // JACSON mas poderia
-        // JsonParser mas usei var
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        ExtratorDeConteudoIMDB extrator = new ExtratorDeConteudoIMDB();
+        
+        //String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
+        //ExtratorDeConteudoDaNasa extrator = new ExtratorDeConteudoDaNasa();
+        
+        
+        //buscar os dados
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         // exibir e manipular os dados
-        //criando a fabrica de fora por otimização
+        List<Conteudo> conteudos = extrator.extraiConteudos(json);
+
+        // criando a fabrica de fora por otimização
         var fabrica = new FabricaDeFigurinhas();
 
-        for (Map<String, String> filme : listaDeFilmes) {
+        for (int i = 0; i < 3; i++) {
 
-            //gerando figurinhas de todos os filmes
-            String urlImage = filme.get("image");
-            String titulo = filme.get("title");
+            Conteudo conteudo = conteudos.get(i);
 
-            InputStream inputStream = new URL(urlImage).openStream();
-            String nomeArquivo = titulo + ".png";
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "saida/" + conteudo.getTitulo() + ".png";
 
             fabrica.cria(inputStream, nomeArquivo);
-            
-            //imprimindo dados de todos os filmes
-            System.out.println("\u001b[1mTítulo:\u001b[m " + filme.get("title"));
-            System.out.println("\u001b[1mPoster:\u001b[m " + filme.get("image"));
-            System.out.println("\u001b[0;103m \u001b[30mClassificação: " + filme.get("imDbRating") + " \u001b[m");
+
+            System.out.println(conteudo.getTitulo());
             System.out.println();
-
-            //exibir estrelas de acordo com a avaliação
-            String ratingString = filme.get("imDbRating");
-			double ratingNumber = Double.parseDouble(ratingString);
-			int ratingRoundedUp = (int) Math.round(ratingNumber);
-			for(int i = 0; i < ratingRoundedUp; i ++){
-				System.out.print("⭐");
-			}
-			System.out.println(" ");
-			System.out.println(" ");
-
         }
     }
 }
---
+++ src/ExtratorDeConteudoDaNasa.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ExtratorDeConteudoDaNasa implements ExtratorDeConteudo{
+    
+    public List<Conteudo> extraiConteudos(String json){
+        
+        //extrair so os dados que interessam
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeAtributos = parser.parse(json);
+        List<Conteudo> ListaDeConteudos = new ArrayList<>();
+
+        //popular a lista de conteudos
+        for(Map<String, String> atributos : listaDeAtributos){
+
+            String titulo = atributos.get("title");
+            String urlImage = atributos.get("url");
+
+            var conteudo = new Conteudo(titulo, urlImage);
+            ListaDeConteudos.add(conteudo);
+        }
+
+        return ListaDeConteudos;
+    }
+}
